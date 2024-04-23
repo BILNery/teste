98b4797aef015e42ef6940a1439b81951cf484f5
BILN <bnery.biln@gmail.com>
2024-04-23 20:17:35 -0300

    'version1'

--- app.py
+++ app.py
@@ -1,36 +1,20 @@
-#CRIANDO O APP#
-
-import streamlit as st
-
-st.header('Jogando uma moeda')
-
-st.write('Ainda não é um aplicativo funcional. Em construção.')
-
-#Adicionando o controle deslizante e o botão ao programa#
-
-import streamlit as st
-
-st.header('Jogando uma moeda')
-
-number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
-start_button = st.button('Executar')
-
-if start_button:
-    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
-
-st.write('Ainda não é um aplicativo funcional. Em construção.')
-
-#adicionar os resultados das tentativas à interface do usuário, calcular a média e exibir como ela muda à medida que as tentativas progridem#
-
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+# estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Jogando uma moeda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # função que emula o lançamento de uma moeda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -53,3 +37,17 @@ start_button = st.button('Executar')
 
 if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
